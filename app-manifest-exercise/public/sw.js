079238f3a0eafe2cff302f6affcd8daf6515996e
Luis Cabello de Alba Fernandez <luisjavier.cabellodealba@bbva.com>
2019-11-30 17:24:20 +0100

    Non-lifecycle events

--- app-manifest-exercise/public/sw.js
+++ app-manifest-exercise/public/sw.js
@@ -6,3 +6,8 @@ self.addEventListener('active', function(event) {
   console.log('[Servier Worker] Activating Service Worker...' + event);
   return self.ClientRectList.claim();
 });
+
+self.addEventListener('fetch', function(event) {
+  console.log('[Service worker] Fetching someting...', event);
+  event.respondWith(fetch(event.request));
+});
